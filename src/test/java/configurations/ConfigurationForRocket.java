64889976dae9860c3f603c211c09dce6466d724e
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-05-31 00:17:55 +0500

    Зарефакторил конфигурацию для тестов Rocket

--- src/test/java/configurations/ConfigurationForRocket.java
+++ src/test/java/configurations/ConfigurationForRocket.java
@@ -1,4 +1,4 @@
-package tests;
+package configurations;
 
 import io.restassured.RestAssured;
 import io.restassured.config.DecoderConfig;
@@ -8,14 +8,10 @@ import io.restassured.mapper.ObjectMapperType;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-public class Configuration implements BeforeAllCallback {
+public class ConfigurationForRocket implements BeforeAllCallback {
     @Override
     public void beforeAll(ExtensionContext extensionContext) throws Exception {
         RestAssured.baseURI = "https://api.spacexdata.com";
         RestAssured.basePath = "/v3";
-        RestAssured.config = RestAssuredConfig.config()
-                .decoderConfig(new DecoderConfig("UTF-8"))
-                .objectMapperConfig(new ObjectMapperConfig(ObjectMapperType.JACKSON_2));
-
     }
 }
