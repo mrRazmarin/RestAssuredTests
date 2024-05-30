2317026d1bffd0b474c1120138d7f37fa7b06857
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-05-31 01:07:09 +0500

    Добавил еще один тест для массива объектов Core

--- src/test/java/tests/CoreTests.java
+++ src/test/java/tests/CoreTests.java
@@ -2,11 +2,18 @@ package tests;
 
 import configurations.ConfigurationForCore;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import objectsJSON.Core.CoreObject.Core;
+import objectsJSON.Core.CoreObject.underCore.Mission;
+import objectsJSON.Rocket.RocketObject.Rocket;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
 
 import static io.restassured.RestAssured.given;
 
@@ -24,5 +31,22 @@ public class CoreTests {
         String coreSerialFromObject = core.getCoreSerial();
         System.out.println(coreSerialFromObject);
         Assertions.assertEquals("B1032", coreSerialFromObject);
+    }
+    @Test
+    @DisplayName("Get first object from json-request and checking Mission.name")
+    public void getFirstObjectFromJsonRequestAndCheckMissionName() {
+        String endpoint = "";
+        String actualMissionName = "FalconSat";
+        Response coresResponse = given().when().contentType(ContentType.JSON)
+                .get(endpoint)
+                .then().log().all()
+                .extract().response();
+        List<Mission> missions = Arrays.asList(coresResponse.as(Core[].class))
+                .stream()
+                .findFirst()
+                .get()
+                .getMissions();
+        String nameMission = missions.get(0).getName();
+        Assertions.assertEquals(nameMission, actualMissionName);
     }
 }
