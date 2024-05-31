93a4795835285bcc797d7af6c200a7f54209c333
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-05-31 20:05:14 +0500

    Переписал тест, разделил на шаги и сделал параметризированным

--- src/test/java/tests/RocketTests.java
+++ src/test/java/tests/RocketTests.java
@@ -1,37 +1,36 @@
 package tests;
 
 
+import configurations.AllureConfiguration;
 import configurations.ConfigurationForRocket;
-import io.restassured.http.ContentType;
+import helpers.EndPointsForTests;
+import io.qameta.allure.*;
 import objectsJSON.Rocket.RocketObject.Rocket;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import steps.RocketSteps;
 
 import java.util.List;
 
-import static io.restassured.RestAssured.given;
-
-@ExtendWith(ConfigurationForRocket.class)
+@ExtendWith({ConfigurationForRocket.class})
+@DisplayName("Rocket's tests")
 public class RocketTests {
-    @Test
-    @DisplayName("Check name country to first rocket")
-    public void getRockets() {
-        String getEndPoint = "/rockets";
-        List<Rocket> rocketList = given()
-                .when().contentType(ContentType.JSON)
-                .get(getEndPoint)
-                .then().log().all()
-                .extract().body().jsonPath().getList("",Rocket.class);
+    private final RocketSteps rocketSteps = new RocketSteps();
+    private final EndPointsForTests endPointsForTests = new EndPointsForTests();
 
-        String rocketCountryOne = rocketList.stream()
-                .filter(x -> x.getId() == 1)
-                .findFirst().get().getCountry();
-        System.out.println("OUTPUT: " + rocketCountryOne);
-        Assertions.assertEquals("Republic of the Marshall Islands", rocketCountryOne);
+
+    @ParameterizedTest(name = "Check name country {0} in the list rockets, with id = {1}")
+    @MethodSource("helpers.DataForTests#getNamesItems")
+    @Epic("API")
+    @Story("API-SpaceX")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("Mikhail Salnikov")
+    @Issue("Rocket's")
+    public void getRockets(String countryName, Long id) {
+        List<Rocket> listWithAllRocket = rocketSteps.openRocket(endPointsForTests.getEndPointAllRocket());
+        Rocket rocket = rocketSteps.getRocketFromListById(listWithAllRocket, id);
+        rocketSteps.checkCountryNameOfTheRocket(rocket, countryName);
     }
-
-
 }
